b45204e708b549763555988cfa0478e46b528dff
Tran Thanh Dan <tranthanhdan.se@gmail.com>
2026-05-03 20:24:41 +0700

    feat:add frontend structure

---
+++ frontend/src/pages/Example.jsx
@@ -0,0 +1,10 @@
+function Example() {
+    return (
+        <div>
+            <h1>Example Layout</h1>
+            <p>This is an example layout component.</p>
+        </div>
+    );
+}
+
+export default Example;
